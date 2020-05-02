9b2ef1222184ef340a3dc34d0b2869ca9ad9c88f
Matthias Mees <mm@yellowled.de>
2020-05-02 20:27:51 +0200

    Finetune date filters

--- src/_11ty/filters.js
+++ src/_11ty/filters.js
@@ -2,15 +2,10 @@ const { DateTime } = require('luxon');
 
 module.exports = {
     dateFormat: function (date, format = 'dd.MM.yyyy') {
-        return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(
-            String(format)
-        );
+        return DateTime.fromJSDate(date).toFormat(String(format));
     },
 
     dateISO: function (date) {
-        return DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
-            includeOffset: false,
-            suppressMilliseconds: true,
-        });
+        return DateTime.fromJSDate(date).toISO();
     },
 };
